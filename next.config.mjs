2d6770e8d594ff1da935cc0349f7c0a9acb44cf7
Pragya79645 <pragya220898@gmail.com>
2026-04-21 17:43:28 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ const nextConfig = {
     unoptimized: true,
   },
   experimental: {
+    turbo: false,
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
